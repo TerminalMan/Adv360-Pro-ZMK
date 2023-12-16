7f5c8c5b1052be0f579f70c651536fa32dd044de
analoq <>
2023-12-16 17:36:17 +0100

    fixed missing key, added left/right flashing to flash.sh

--- flash.sh
+++ flash.sh
@@ -1,8 +1,16 @@
 drive=$(lsblk | grep 32.1M)
 drive=${drive:0:3}
 
-echo $drive
+if [ $1 == "left" ]
+then
+	sudo mount /dev/$drive /mnt/sdx
+	sudo cp firmware/*left.uf2 /mnt/sdx/left.uf2
+fi
 
-sudo mount /dev/$drive /mnt/sdx
-sudo cp firmware/*left.uf2 /mnt/sdx/left.uf2
+if [ $1 == "right" ]
+then
+	sudo mount /dev/$drive /mnt/sdx
+	sudo cp firmware/*right.uf2 /mnt/sdx/right.uf2
+fi
+
 
